529fc2c21f180b2a76f084ace5412071f3a321cf
Parnian Yousefi Sabet <parnian.yousefi.s@gmail.com>
2024-03-09 09:42:50 +0330

    Create 2885.cpp
    Solution to https://quera.org/problemset/2885

---
+++ 2885.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i, n;
+    cin>>n;
+    
+    cin>>n;
+    
+    for(i= 0 ; i < n ; i++){
+        cout<<"man khoshghlab hastam"<<endl;
+    }
+    
+    return 0;
+}
