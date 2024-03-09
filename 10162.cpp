622e241b94a00de1f3a7b2be3e670f4bb52a5be0
Parnian Yousefi Sabet <parnian.yousefi.s@gmail.com>
2024-03-09 09:56:22 +0330

    Create 10162.cpp
    Solution to https://quera.org/problemset/10162

---
+++ 10162.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int k;
+    
+    cin>>k;
+    
+    if(k % 2 == 0)
+        cout<<"Bala Barare";
+        
+    else
+        cout<<"Payin Barare";
+    
+    return 0;
+}
